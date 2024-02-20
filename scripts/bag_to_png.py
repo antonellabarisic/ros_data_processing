bc1fa2653ce99ae7aa4b6b4de5a3e3891f107640
antonellabarisic <antonella.barisic@fer.hr>
2024-02-20 19:32:51 +0100

    Update script for bag2png

--- scripts/bag_to_png.py
+++ scripts/bag_to_png.py
@@ -10,32 +10,43 @@ import time
 class ImageSaver:
     def __init__(self):
         self.bridge = CvBridge()
-        # self.image_sub = rospy.Subscriber("/red/camera/image_color", Image, self.callback)
-        self.image_sub = rospy.Subscriber("/throttled_camera_topic", Image, self.callback)
-        self.destination_path = "/home/antonella/Datasets/mbzirc2024/abu_dhabi_yellow_boat/1501_from_bags"
-        self.save_rate = 0.1  # Save an image every 5 seconds
+        self.image_sub = rospy.Subscriber("/hawkred/camera/color/image_raw", Image, self.callback)
+        self.destination_path = "/home/antonella/Datasets/mbzirc2024/small_large_object/images_from_bags"
+        self.save_rate = 0.5  # Save an image every 0.1 seconds
         self.last_save_time = 0
-        self.rotate_180 = True
+        self.rotate_180 = False
 
         if not os.path.exists(self.destination_path):
             os.makedirs(self.destination_path)
 
-    def callback(self, data):
-        # current_time = time.time()
-        # if current_time - self.last_save_time >= self.save_rate:
-        try:
-            if self.rotate_180:
-                    cv_image = cv2.rotate(self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8"), cv2.ROTATE_180)
-            else:
-                cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            # cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
+        self.img_count = 0
 
-        ros_time_int = int(rospy.get_time())
-        img_name = "image_{}.png".format(ros_time_int)
-        cv2.imwrite(os.path.join(self.destination_path, img_name), cv_image)
-        # self.last_save_time = current_time
+    def callback(self, data):
+        current_time = time.time()  # Get current time in seconds
+        if current_time - self.last_save_time >= self.save_rate:
+            try:
+                if self.rotate_180:
+                        cv_image = cv2.rotate(self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8"), cv2.ROTATE_180)
+                else:
+                    cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+                # cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            except CvBridgeError as e:
+                print(e)
+
+            # Name image based on rospy time
+            current_ros_time = rospy.get_rostime()  # This returns a rospy.Time instance
+            ros_time_sec = current_ros_time.secs
+            ros_time_nsec = current_ros_time.nsecs
+
+            img_name = "image_{}_{}.png".format(ros_time_sec, ros_time_nsec)
+
+            cv2.imwrite(os.path.join(self.destination_path, img_name), cv_image)
+            self.img_count += 1
+            self.last_save_time = current_time  # Update the last save time
+
+            print("Saved image named {}".format(img_name))
+            print("Saved image number {}".format(self.img_count))
+
 
 def main():
     rospy.init_node('image_saver', anonymous=True)
